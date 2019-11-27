2902ff6965e486bb28e7d5a48297e6d410439d88
Alassane Ndoye <45608228+aloundoye@users.noreply.github.com>
2019-11-27 17:37:50 +0000

    ajout de quelque caractere sur la liste

--- code/maincode.js
+++ code/maincode.js
@@ -1,7 +1,7 @@
 var afficheCode = document.querySelector('#affCode');
 var buttongen = document.getElementById('buttongen');
 var caracteres =['A','B','C','D', 'E','F','G', 'H', 'I','J', 'K', 'L',
- 'M','N','O','P','Q', 'R', 'S', 'T', 'U','V','W','X','Y','Z','1', '2', '3', '4', '5', '6', '7', '8', '9'];
+ 'M','N','O','P','Q', 'R', 'S', 'T', 'U','V','W','X','Y','Z','0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function genereCode(){
     let code ="";
